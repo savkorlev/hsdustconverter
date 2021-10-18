0089475a6355578bc6f5f8b82d56a7f9b9fef7f9
savkorlev <evglev997@gmail.com>
2021-10-18 16:15:22 +0200

    Update test_hsdustconverter.py

--- tests/test_hsdustconverter.py
+++ tests/test_hsdustconverter.py
@@ -8,21 +8,7 @@ import pytest
 from hsdustconverter import hsdustconverter
 
 
-@pytest.fixtur
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
+def test_content():
     expectedCommons = 100
     expectedRares = 200
     expectedEpics = 300
